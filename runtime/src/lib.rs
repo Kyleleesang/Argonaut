07006d226a19c4b53a8869b991b52907b265bea8
Kyle <kyleleesang117@gmail.com>
2023-05-28 02:38:54 -0400

    removed template

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -42,8 +42,7 @@ use pallet_transaction_payment::CurrencyAdapter;
 pub use sp_runtime::BuildStorage;
 pub use sp_runtime::{Perbill, Permill};
 
-/// Import the template pallet.
-pub use pallet_template;
+
 
 /// An index to a block.
 pub type BlockNumber = u32;
@@ -263,10 +262,7 @@ impl pallet_sudo::Config for Runtime {
 	type Call = Call;
 }
 
-/// Configure the pallet-template in pallets/template.
-impl pallet_template::Config for Runtime {
-	type Event = Event;
-}
+
 /* 
  Replacing Runtime config with Argonaut Config a WIP
 
@@ -298,8 +294,6 @@ construct_runtime!(pub struct Runtime where
 		Balances: pallet_balances,
 		TransactionPayment: pallet_transaction_payment,
 		Sudo: pallet_sudo,
-		// Include the custom logic from the pallet-template in the runtime.
-		TemplateModule: pallet_template,
 	}
 );
 
@@ -344,7 +338,6 @@ mod benches {
 		[frame_system, SystemBench::<Runtime>]
 		[pallet_balances, Balances]
 		[pallet_timestamp, Timestamp]
-		[pallet_template, TemplateModule]
 	);
 }
 
